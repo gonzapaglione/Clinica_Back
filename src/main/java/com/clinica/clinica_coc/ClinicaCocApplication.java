0c93e156222d599190ba8b75a4dee481d09f6bfe
lourdesgv19 <guerrierivluli@gmail.com>
2025-10-26 00:50:27 -0300

    Expiracion del token modificada

--- src/main/java/com/clinica/clinica_coc/ClinicaCocApplication.java
+++ src/main/java/com/clinica/clinica_coc/ClinicaCocApplication.java
@@ -1,11 +1,8 @@
 package com.clinica.clinica_coc;
 
-import java.util.TimeZone;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ClinicaCocApplication {
