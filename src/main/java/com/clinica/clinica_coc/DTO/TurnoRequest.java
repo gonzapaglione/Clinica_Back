c1aebe7ab29c8907e69f7fbdffb69223d0f906c2
lourdesgv19 <guerrierivluli@gmail.com>
2025-10-27 11:57:03 -0300

    Guardando cambio turno request

--- src/main/java/com/clinica/clinica_coc/DTO/TurnoRequest.java
+++ src/main/java/com/clinica/clinica_coc/DTO/TurnoRequest.java
@@ -2,6 +2,8 @@ package com.clinica.clinica_coc.DTO;
 
 import java.time.LocalDateTime;
 
+import com.clinica.clinica_coc.models.MotivoConsultaEnum;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +18,7 @@ public class TurnoRequest {
     private Long idOdontologo;
     private LocalDateTime fechaHora;
     private String estadoTurno;
-    private String motivoConsulta;
+    private MotivoConsultaEnum motivoConsulta;
     private String tratamiento;
     private String evolucion;
 }
